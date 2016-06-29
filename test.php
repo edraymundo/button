59fb3d8d12645d24edb107ef26993c05f41fa8d6
edraymundo <edraymundo@users.noreply.github.com>
2016-06-29 13:47:12 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "sending mail";
+
+$from = "button@edraymundo.com";
+$to = $_REQUEST['phone'];
+$mesg = $_REQUEST['mesg'];
+
+$code = $_REQUEST['code'];
+
+if($code == 'bahay0210'){
+
+        $headers = 'From: '.$from. ' <'.$from.'>' . "\r\n" .
+                   'Reply-To: '.$from. "\r\n" .
+                    'X-Mailer: PHP/' . phpversion();
+
+        $ret = mail($to, '',$mesg,$headers,"-f"."$from");
+}
