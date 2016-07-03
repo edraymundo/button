aea10c922b50a1a213248c989dcb098013f8b324
edraymundo <edraymundo@users.noreply.github.com>
2016-07-03 14:57:51 -0700

    updated to pragmatic1.com

--- mail.php
+++ mail.php
@@ -1,18 +1,18 @@
 <?php
-
-echo "sending mail";
-
+echo "Sending mail....<br>";
 $from = "button@pragmatic1.com";
 $to = $_REQUEST['phone'];
 $mesg = $_REQUEST['mesg'];
-
 $code = $_REQUEST['code'];
-
 if($code == 'bahay0210'){
-
-        $headers = 'From: '.$from. ' <'.$from.'>' . "\r\n" .
-                   'Reply-To: '.$from. "\r\n" .
-                    'X-Mailer: PHP/' . phpversion();
-
-        $ret = mail($to, '',$mesg,$headers,"-f"."$from");
+        try{
+                $headers = 'From: '.$from. ' <'.$from.'>' . "\r\n" .
+                           'Reply-To: '.$from. "\r\n" .
+                           'X-Mailer: PHP/' . phpversion();
+                mail($to, '',$mesg,$headers,"-f"."$from");
+        }catch (Exception $e){
+                echo "Failed: $e->getMessage()<br>";
+                exit;
+        }
+        echo "Success";
 }
